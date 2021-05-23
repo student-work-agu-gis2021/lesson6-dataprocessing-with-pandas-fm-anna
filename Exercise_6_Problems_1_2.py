d49aff13afdc48fcec36dea30f85c6c72f3d09fd
fmanna <>
2021-05-23 07:59:35 +0000

    I upgraded pip-21.1.1 from pip-19.3.1.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -61,7 +61,6 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 day_count = None 
 #YOUR CODE HERE 4
 day_count = len(data)
-
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print("Number of days:", day_count)
